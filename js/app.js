b45fcc4002725fab9fb292d2515cc85ceffae09d
M LOW <mlow991@users.noreply.github.com>
2015-11-15 23:00:40 -0800

    Map center now adjusts to window resize.  Will now work on zoom.

--- js/app.js
+++ js/app.js
@@ -6,15 +6,24 @@ var defaultLocations = [{lat: 21.2759257, lng: -157.8145445}]
 var map;
 
 function initMap() {
-	map = new google.maps.Map(document.getElementById('map'), {
+	var mapOptions = {
 		center: myLatLng,
 		scrollwheel:false,
 		zoom: 11
-	});
+	};
+	map = new google.maps.Map(document.getElementById('map'), mapOptions);
 	var marker = new google.maps.Marker({
 		map: map,
 		position: defaultLocations[0]
 	});
+	google.maps.event.addDomListener(window, 'load', initMap);
+	google.maps.event.addDomListener(window, 'resize', function() {
+		var c = map.getCenter();
+		google.maps.event.trigger(map, 'resize');
+		map.setCenter(c);
+	});
 }
+
+
 
 //https://maps.googleapis.com/maps/api/geocode/json?address=3308+Kanaina+Avenue+Honolulu,+HI+96815&key=AIzaSyCkcvMu_0Ar7_Xv3R3MB6-Ffp_Gxq9Di9s
