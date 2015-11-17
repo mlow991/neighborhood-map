ab275c4e92df70fc1846a574b78ee908097763a0
M LOW <mlow991@users.noreply.github.com>
2015-11-17 02:12:40 -0800

    UI css updated, loading all places in the placesView

--- js/app.js
+++ js/app.js
@@ -23,12 +23,22 @@ function initMap() {
 	});
 }
 
+function placesViewModel() {
+	var self = this;
+
+	self.places = ko.observableArray([
+	{name: "Rainbow Drive-In", type: "restaruant", coordinates: {lat: 21.2759257, lng: -157.8145445}, description: "Description here"},
+	{name: "Big Wave Shrimp Truck", type: "restaraunt", coordinates: {lat: 21.579469, lng: -158.105532}, description: "Description here"}
+]);
+
+}
+
 var viewModel = {
 	test: ko.observable("Test: "),
 	phrase: ko.observable(123)
 };
 
-ko.applyBindings(viewModel);
+ko.applyBindings(new placesViewModel());
 
 
 
