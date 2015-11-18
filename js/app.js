322d34e5561622a04eb941c7e91bdd3142624ce7
M LOW <mlow991@users.noreply.github.com>
2015-11-17 18:10:44 -0800

    Testing out two viewmodels and the textInput observable

--- js/app.js
+++ js/app.js
@@ -27,18 +27,32 @@ function placesViewModel() {
 	var self = this;
 
 	self.places = ko.observableArray([
-	{name: "Rainbow Drive-In", type: "restaruant", coordinates: {lat: 21.2759257, lng: -157.8145445}, description: "Description here"},
-	{name: "Big Wave Shrimp Truck", type: "restaraunt", coordinates: {lat: 21.579469, lng: -158.105532}, description: "Description here"}
-]);
+		{name: "Rainbow Drive-In", type: "restaruant", coordinates: {lat: 21.2759257, lng: -157.8145445}, description: "Description here"},
+		{name: "Big Wave Shrimp Truck", type: "restaraunt", coordinates: {lat: 21.579469, lng: -158.105532}, description: "Description here"}
+	]);
 
+	self.print = function() {
+		console.log("clicked");
+	}
 }
+
+function searchViewModel() {
+	var self = this;
+
+	self.search = ko.observable('');
+
+	self.log = function() {
+		console.log(self.search());
+	}
+};
 
 var viewModel = {
 	test: ko.observable("Test: "),
 	phrase: ko.observable(123)
 };
 
-ko.applyBindings(new placesViewModel());
+ko.applyBindings(new placesViewModel(), document.getElementById('placesField'));
+ko.applyBindings(new searchViewModel(), document.getElementById('searchField'));
 
 
 
